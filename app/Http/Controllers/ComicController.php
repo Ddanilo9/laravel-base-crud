1ec1f1a2a11ff70009f739e70a3bf904283e3356
Ddanilo9 <interiordanilo3@gmail.com>
2022-10-14 17:17:53 +0200

    vista show added

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -50,7 +50,9 @@ class ComicController extends Controller
      */
     public function show($id)
     {
-        dd($id);
+        $comic = Comic::findOrFail($id);
+
+        return view('comics.show', compact('comic'));
     }
 
     /**
---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<img src="{{$comic->thumb}}" alt="">
+<h1>{{$comic->title}}</h1>
+<h4>serie: <span>{{$comic->series}}</span></h4>
+    
+@endsection
